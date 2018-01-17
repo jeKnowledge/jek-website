4b7c54e8b281a2331024e185adbc1e31936cbd11
simaonogueira101 <simao.nogueira66@gmail.com>
2018-01-17 18:05:22 +0000

    Adds serve-favicon to package json and further styles hero/navbar.

--- public/js/hero-script.js
+++ public/js/hero-script.js
@@ -13,6 +13,7 @@ var weAre = [
 ];
 
 var selector = 0;
+var actionActive = false;
 
 setInterval(function(){
   if(selector < weAre.length) {
@@ -25,15 +26,42 @@ setInterval(function(){
     }
     setTimeout(function(){
       document.querySelector("#we").classList.remove("active");
-    }, 2500);
+    }, 3000);
   }
-}, 3000);
+}, 3500);
+
 
 document.querySelector(".c-hero-ov-3").addEventListener('click', heroAction);
 function heroAction() {
-  window.scroll({
-    top: document.querySelector(".c-contact").offsetTop,
-    left: 0,
-    behavior: 'smooth'
-  });
+  if(!actionActive) {
+    window.scroll({
+      top: document.querySelector(".c-contact").offsetTop,
+      left: 0,
+      behavior: 'smooth'
+    });
+  } else {
+    navbar();
+  }
+}
+
+window.onscroll = function() {
+  if (window.scrollY > document.querySelector(".c-hero-ov-action").offsetTop - 24) {
+    document.querySelector(".c-hero-ov-action").classList.add("active");
+    document.querySelector(".c-hero-ov-3").classList.add("active");
+    document.querySelector(".c-hero-ov-action").classList.remove("still");
+    document.querySelector("#action-text").innerHTML = "jeKnowledge";
+    var actionDisplaceText = window.innerWidth/2 - document.querySelector("#action-text").offsetWidth/2 - 32;
+    var actionDisplaceBall = window.innerWidth/2 - document.querySelector(".c-hero-ov-3").offsetWidth/2 - 24;
+    document.querySelector(".c-hero-ov-action").style.transform = "translateX(-" + actionDisplaceText + "px)";
+    document.querySelector(".c-hero-ov-action p").style.opacity = "1";
+    document.querySelector(".c-hero-ov-3").style.transform = "translateX(" + actionDisplaceBall + "px)";
+    document.querySelector(".c-hero-ov-3").style.opacity = "1";
+    actionActive = true;
+  }
+};
+
+function navbar() {
+  document.querySelector(".navbar").classList.toggle("active");
+  document.querySelector(".c-hero-ov-3").classList.toggle("selected");
+  document.querySelector(".c-hero-ov-action").classList.toggle("selected");
 }
