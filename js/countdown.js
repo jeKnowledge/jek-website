4114ab17f26709e6dcb7b6782707e94c3d5cc092
mariamachado6 <molimpiadias@gmail.com>
2019-09-24 17:30:19 +0100

    Add a countdown timer on recruitment page

---
+++ js/countdown.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Sep 30, 2019 23:59:00").getTime();
+var countDownfunction = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  if(distance < 0) {
+    clearInterval(countDownfunction);
+    document.getElementById()("demo").innerHTML = "Happy days";
+  }
+},1000);
