3879594f60ef6db34caf352d24246f0c07b7ff1b
Bernardo Prior <bernardo.prior@hotmail.com>
2021-03-28 20:32:55 +0100

    2021 recruitment

--- js/countdown.js
+++ js/countdown.js
@@ -1,26 +1,24 @@
-var countDownDate = new Date("Apr 06, 2020 23:59:00").getTime();
-var countDownfunction = setInterval(function() {
+var countDownDate = new Date("Apr 10, 2021 23:59:00").getTime();
+var countDownfunction = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  if (countDownDate<now){
+  if (countDownDate < now) {
     document.getElementById("days").innerHTML = 0;
     document.getElementById("hours").innerHTML = 0;
     document.getElementById("minutes").innerHTML = 0;
     document.getElementById("seconds").innerHTML = 0;
-  }
-  else{
+  } else {
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
   }
-  if(distance < 0) {
+  if (distance < 0) {
     clearInterval(countDownfunction);
     document.getElementById()("demo").innerHTML = "Happy days";
   }
-
-},1000);
+}, 1000);
